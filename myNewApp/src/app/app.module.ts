f60555032c72c937c756c254d5b5558ac1e00b98
bishnu <bishnu.gurung@selise.ch>
2022-06-13 10:36:33 +0600

    initial changes

--- myNewApp/src/app/app.module.ts
+++ myNewApp/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import {Component} from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
